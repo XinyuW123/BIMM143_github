995e3856e73acb3762fbbeab13e16f5444059e7b
Xinyu Wen <x5wen@ucsd.edu>
2025-02-25 11:50:42 -0800

    Add class folder 3

---
+++ Class4Work/class 4.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "BIMM 143 Class04: Intro to R Lab"
+#' author: "Xinyu Wen (A17115443)"
+#' date: "January 16th, 2025"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+
+# make thi nicer
+plot(x, sin(x), col="pink", type = "l", lwd=4)
